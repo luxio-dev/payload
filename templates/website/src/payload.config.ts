a95de53b8694864610d1f0bba0f0f329a7012863
Luxio <admin@luxio.au>
2025-05-27 16:30:43 +1000

    feat: Configure Supabase database connection and fix initial setup

--- templates/website/src/payload.config.ts
+++ templates/website/src/payload.config.ts
@@ -1,5 +1,6 @@
 // storage-adapter-import-placeholder
-import { mongooseAdapter } from '@payloadcms/db-mongodb' // database-adapter-import
+// import { mongooseAdapter } from '@payloadcms/db-mongodb' // database-adapter-import
+import { postgresAdapter } from '@payloadcms/db-postgres'
 
 import sharp from 'sharp' // sharp-import
 import path from 'path'
@@ -59,16 +60,21 @@ export default buildConfig({
   },
   // This config helps us configure global or default features that the other editors can inherit
   editor: defaultLexical,
+
   // database-adapter-config-start
-  db: mongooseAdapter({
-    url: process.env.DATABASE_URI,
+  db: postgresAdapter({
+    pool: {
+      connectionString: process.env.DATABASE_URI,
+    },
   }),
   // database-adapter-config-end
+
   collections: [Pages, Posts, Media, Categories, Users],
   cors: [getServerSideURL()].filter(Boolean),
   globals: [Header, Footer],
   plugins: [
     ...plugins,
+
     // storage-adapter-placeholder
   ],
   secret: process.env.PAYLOAD_SECRET,
